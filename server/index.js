4eb8daf47d82bda90cca298ab3ce139156e00cde
dongls <173110115@qq.com>
2018-08-30 10:55:02 +0800

    koa-bodyparser

--- server/index.js
+++ server/index.js
@@ -2,12 +2,16 @@ const path = require('path')
 
 const Koa = require('koa');
 const koaStatic = require('koa-static')
+const bodyParser = require('koa-bodyparser');
 
 const server = new Koa();
 const router = require('./routes')
 
 server.init = function(){
-    //静态资源
+  //参数解析
+  server.use(bodyParser());
+
+  //静态资源
   server.use(koaStatic(path.resolve(__dirname, '../public'), {index: '_index.html'}));
 
   //路由
