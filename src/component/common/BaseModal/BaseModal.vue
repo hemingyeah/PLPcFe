c1392a9665907d09b57ef63bdace5cc426e94160
dongls <173110115@qq.com>
2018-11-05 12:07:22 +0800

    新增BaseModal的appendToBody属性，用于将modal插入到body中

--- src/component/common/BaseModal/BaseModal.vue
+++ src/component/common/BaseModal/BaseModal.vue
@@ -48,7 +48,11 @@ export default {
       type: Boolean,
       default: false
     },
-    bodyClass: String
+    bodyClass: String,
+    appendToBody: { //是否将弹窗插入body中
+      type: Boolean,
+      default: false
+    }
   },
   methods: {
     closed(){
@@ -76,9 +80,18 @@ export default {
   },
   mounted(){
     //document.addEventListener('keydown', this.escClose)
+
+    if (this.appendToBody) {
+      document.body.appendChild(this.$el);
+    }
   },
   destroyed(){
     //document.removeEventListener('keydown', this.escClose)
+
+    // if appendToBody is true, remove DOM node after destroy
+    if (this.appendToBody && this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el);
+    }
   }
 }
 </script>
