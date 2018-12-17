b88bac8d54bc5c282b75709cba3d8b7852eea4a9
dongls <173110115@qq.com>
2018-12-17 10:41:02 +0800

    fix: 修正备件1.0图标显示

--- src/modules/system/frame/model/MenuIcon.js
+++ src/modules/system/frame/model/MenuIcon.js
@@ -3,6 +3,7 @@ export default {
   "M_TASK": "icon-gongdanzhongxin", //工单中心
   "M_CUSTOMER": "icon-kehuxinxi", //客户信息
   "M_VIP_SPAREPART": "icon-beijianguanli", //备件管理
+  "M_SPAREPART": "icon-beijianguanli", //备件1.0
   "M_DASHBOARD": "icon-yunyingfenxi", // 运营分析
   "M_SERVICE": "icon-fuwuguanli", //服务管理
   "M_INFO": "icon-xinxiguanli", //信息管理
