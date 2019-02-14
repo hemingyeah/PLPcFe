53111052eec09e0db8a79ce71b42585534481226
AaronMao <304538821@qq.com>
2019-02-14 08:59:20 +0800

    fix: fix params

--- src/modules/customer/view/components/CustomerRemindTable.vue
+++ src/modules/customer/view/components/CustomerRemindTable.vue
@@ -20,7 +20,7 @@
         :show-overflow-tooltip="column.tooltip"
         :align="column.align">
         <template slot-scope="scope">
-          <template v-if="column.field === 'modalName'">
+          <template v-if="column.field === 'remind.name'">
             <a href="javascript:;" class="edit-btn" @click="$eventBus.$emit('customer_detail_view.update_remind', scope.row)">{{scope.row.remind.name}}</a>
           </template>
           <template v-else-if="column.field === 'remindTime'">
@@ -138,7 +138,7 @@ export default {
     buildColumns() {
       return [{
         label: '提醒名称',
-        field: 'modalName',
+        field: 'remind.name',
         show: true,
         tooltip: true,
         sortable: 'custom',
