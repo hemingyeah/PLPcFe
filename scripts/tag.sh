12d9ac68f2667d82e25cc41533bd9e298369f749
AaronMao <304538821@qq.com>
2019-04-08 09:39:25 +0800

    feat: add shell for git tag

---
+++ scripts/tag.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# print variable
+ echo $1
+ echo $2
+ echo "$branchName"
+
+# check variable
+ if [ -z $1 ]; then
+  echo "warn: no version";
+  exit;
+fi
+
+ if [ -z $2 ]; then
+  echo "warn: no message";
+  exit;
+fi
+
+# check working area
+if [ -n "$(git status --porcelain)" ]; then
+  echo "warn: working area not clean";
+  exit;
+fi
+
+# update dev
+branchName=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+if [ "$branchName" == "dev" ]; then
+  git pull --rebase
+else
+  git checkout dev && git pull --rebase
+fi
+
+# update master
+git checkout master
+git pull --rebase
+git merge dev
+git push
+
+# add tag
+git tag -a $1 -m $2
+git push origin $1
+
+#
+git checkout dev
+git push
+
+echo "end"
+
+# catch error
+
+
