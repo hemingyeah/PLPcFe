aed4b74ab5ab14a488de0aae62b2e2defa0f57e9
zhangaj <mfing_zhang@163.com>
2019-05-15 16:51:53 +0800

    fix: 添加配置

---
+++ script/config/zhangaj.js
@@ -0,0 +1,8 @@
+module.exports = {
+  targetRootPath: '/Users/hbc/SHB/web',
+  loginUser: { // 登录用户
+    // 董立石 - 众联成业测试用例企业
+    userId: 'cf12d356-4130-11e7-a318-00163e304a25',
+    tenantId: '7416b42a-25cc-11e7-a500-00163e12f748'
+  }
+}
