151753dbc149d05382b53f9e0b04715996eefa38
hbc <hbc1124778999@live.com>
2019-05-27 17:42:38 +0800

    fix: 优化 触发input事件会获取中文输入值。

--- src/component/form/components/FormText/FormText.vue
+++ src/component/form/components/FormText/FormText.vue
@@ -3,7 +3,9 @@
     <input
       type="text"
       :value="value"
-      @input="input"
+      @compositionstart="compositionstart"
+      @compositionend="compositionend"
+      @input="inputEvent"
       :placeholder="placeholder"
       :id="`form_${field.fieldName}`" 
       autocomplete="off"/>
@@ -21,7 +23,30 @@ export default {
       type: String,
       default: ''
     }
-  }
+  },
+  data() {
+    return {
+      // 是否是输入中文
+      isInputZh: false,
+    }
+  },
+  methods: {
+    /* 输入文字之前 */
+    compositionstart(event) {
+      this.isInputZh = true;
+    },
+    /* 输入文字之后 */
+    compositionend(event) {
+      this.isInputZh = false;
+      this.input(event);
+    },
+    inputEvent(event) {
+      // 如果是在中文输入 return
+      if(this.isInputZh) return
+
+      this.input(event);
+    }
+  },
 }
 </script>
 
