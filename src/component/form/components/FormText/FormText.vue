5648616ab329448f89a1801ad40e16a320dd1d25
dongls <173110115@qq.com>
2019-05-30 12:08:26 +0800

    fix: isInputZh重命名为$isInputZh

--- src/component/form/components/FormText/FormText.vue
+++ src/component/form/components/FormText/FormText.vue
@@ -27,22 +27,22 @@ export default {
   data() {
     return {
       // 是否是输入中文
-      isInputZh: false,
+      $isInputZh: false,
     }
   },
   methods: {
     /* 输入文字之前 */
     compositionstart(event) {
-      this.isInputZh = true;
+      this.$data.$isInputZh = true;
     },
     /* 输入文字之后 */
     compositionend(event) {
-      this.isInputZh = false;
+      this.$data.$isInputZh = false;
       this.input(event);
     },
     inputEvent(event) {
       // 如果是在中文输入 return
-      if(this.isInputZh) return
+      if(this.$data.$isInputZh) return
 
       this.input(event);
     }
