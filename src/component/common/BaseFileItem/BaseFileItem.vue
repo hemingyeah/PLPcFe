a4314462a4f132ecf695ab99292bcd6f13569755
dongls <173110115@qq.com>
2019-06-06 20:03:14 +0800

    fix: 修正附件下载错误

--- src/component/common/BaseFileItem/BaseFileItem.vue
+++ src/component/common/BaseFileItem/BaseFileItem.vue
@@ -5,7 +5,7 @@
         <img v-if="isImage" :data-origin="file.url" :alt="file.filename">
       </div>
       <div class="base-file-info">
-        <a :href="genDownloadUrl(file.url)">{{file.filename}}</a>
+        <a :href="genDownloadUrl(file)">{{file.filename}}</a>
         <p>
           <span>{{file.fileSize}}</span>
           <button type="button" class="btn-text base-file-del" @click="deleteFile" v-if="!readonly">删除</button>
