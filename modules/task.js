5369a8b1d4578506028f59c18d5985e343517b8a
hbc <hbc1124778999@live.com>
2019-06-13 15:39:47 +0800

    fix: 添加工单fields 入口

--- modules/task.js
+++ modules/task.js
@@ -2,5 +2,9 @@ module.exports = {
   'task.edit': {
     entry: './src/modules/task/edit.js',
     template: 'src/templates/map.html'
+  },
+  'task.fields': {
+    entry: './src/modules/setting/task/fields.js',
   }
+
 }
--- server/routes/task.js
+++ server/routes/task.js
@@ -12,4 +12,11 @@ router.get('/task/create', async ctx => {
   ctx.body = Template.renderWithData('新建工单', {}, script, modConfig.template)
 });
 
+router.get('/task/fields', async ctx => {
+  let modConfig = modules['task.fields'];
+  let script = ['/task.fields.js'];
+  
+  ctx.body = Template.renderWithData('工单设置', {}, script, modConfig.template)
+});
+
 module.exports = router;
