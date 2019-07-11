8db685f0b7b8033ec3d4360b7b1aec2fe889e9ce
sunwj <asswj2011@126.com>
2019-07-11 13:52:33 +0800

    feat: 配置文件

---
+++ script/config/sunwj.js
@@ -0,0 +1,8 @@
+module.exports = {
+  targetRootPath: '/Users/apple/Project/web',
+  loginUser: {
+    // 登录用户
+    userId: 'b9510211-d82f-11e8-b3c6-00163e304a25',
+    tenantId: '7416b42a-25cc-11e7-a500-00163e12f748'
+  }
+};
