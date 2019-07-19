58528bd7910c19f1848d52659e9ca339df804558
hbc <hbc1124778999@live.com>
2019-07-19 15:13:17 +0800

    fix: 🐛 客户字段设置中，添加系统字段提示信息，客户编号、客户、电话
    Closes: #1403

--- src/component/form/components/FormText/extend/CustomerNameExtendSetting.vue
+++ src/component/form/components/FormText/extend/CustomerNameExtendSetting.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="form-setting-panel">
     <h3>系统字段 -- {{field.displayName}}</h3>
+    <p class="form-design-warning" style="margin-bottom: 5px">该字段为系统内置字段，暂不支持修改、删除。</p>
     <!--<div class="form-setting-group">-->
     <!--<textarea placeholder="请在此添加描述信息" rows="3" data-prop="placeHolder" :value="field.placeHolder" @input="updateForDom" :maxlength="placeholderMaxLength"></textarea>-->
     <!--</div>-->
