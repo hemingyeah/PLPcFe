939eb1772b92169e69d3a142de1157244bfeb2f8
zhangaj <mfing_zhang@163.com>
2019-07-23 15:07:38 +0800

    登录配置

--- script/config/zhangaj.js
+++ script/config/zhangaj.js
@@ -1,5 +1,5 @@
 module.exports = {
-  targetRootPath: '/Users/hbc/SHB/web',
+  targetRootPath: '/code/shb-pc-fe',
   loginUser: { // 登录用户
     userId: 'f4cea5f7-49eb-11e9-bfc9-00163e304a25',
     tenantId: '7416b42a-25cc-11e7-a500-00163e12f748'
