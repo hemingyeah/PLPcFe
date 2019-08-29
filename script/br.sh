b42a1be4ab15e0a5a23aa7a84cedcdc1e0c47307
AaronMao <304538821@qq.com>
2019-08-29 14:14:55 +0800

    fix script

--- script/br.sh
+++ script/br.sh
@@ -4,8 +4,21 @@ projects=("shb-pc-fe" "sm4_dd" "sm4-pc" "shb_ssp")
 root="/Users/zhengqiangmao/Aaronmao/workspace/"
 releaseBranchName="release/$(date +'%m%d')"
 echo $1
-# $1：只更新各项目release分支或者bugFix分支
+echo $2
+# $1：前端项目所在路径，不传默认/Users/zhengqiangmao/Aaronmao/workspace/
+# $2：更新只更新各项目release分支或者bugFix分支或者pre_master
 
+if [[ -z $1 ]] || [[ $1 == "m" ]]; then
+    root="/Users/zhengqiangmao/Aaronmao/workspace/"
+fi
+
+if [ "$1" == "h" ]; then
+    root="其他用户路径"
+fi
+
+echo $root
+
+exit;
 for p in "${projects[@]}"
 do
     cd "$root""$p"
@@ -15,7 +28,7 @@ do
       exit;
     fi
 
-    if [[ -z $1 ]] || [[ $1 == "release" ]]; then
+    if [[ -z $2 ]] || [[ $2 == "release" ]]; then
         # 创建release并推送
         git checkout dev
         git pull
@@ -23,7 +36,7 @@ do
         git push --set-upstream origin $releaseBranchName
     fi
 
-    if [[ -z $1 ]] || [[ $1 == "bugFix" ]]; then
+    if [[ -z $2 ]] || [[ $2 == "bugFix" ]]; then
         # 创建bugFix并推送
         git checkout dev
         git pull
@@ -31,7 +44,16 @@ do
         git checkout -b $buFixBranchName
         git push --set-upstream origin $buFixBranchName
     fi
-    
+
+    if [ "$2" == "pre_master" ]; then
+        # 创建pre_master并推送
+        git checkout dev
+        git pull
+        preMasterBranchName="pre_master_$(date +'%m%d')"
+        git checkout -b $preMasterBranchName
+        git push --set-upstream origin $preMasterBranchName
+    fi
+
     # echo "Successfully create branch $releaseBranchName for fe project"
 done
 
