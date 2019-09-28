efd7fe7dec3604b61c586db323d6ac92cbf36a2a
zhangaj <mfing_zhang@163.com>
2019-09-28 10:53:12 +0800

    fix: 分类新建编辑二级分类参数修改

--- src/modules/repository/document/list/component/TypeModal.vue
+++ src/modules/repository/document/list/component/TypeModal.vue
@@ -12,7 +12,7 @@
         </el-form-item>
         <el-form-item label="父级分类" class="type-form">
           <el-select v-model="info.parentId" class="type" clearable :disabled="value.parentId == null">
-            <el-option v-for="item in info.options" :key="item.value" :label="item.name" :value="item.value">
+            <el-option v-for="item in info.options" :key="item.value" :label="item.name" :value="item.id">
             </el-option>
           </el-select>
         </el-form-item>
