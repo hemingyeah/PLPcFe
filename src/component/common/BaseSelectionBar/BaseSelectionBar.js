95fc6dc77d3e3b19a2d6f5c0ec8cf9cd975b2466
hbc <hbc1124778999@live.com>
2019-11-01 00:39:11 +0800

    fix: 修改base-selection-bar支持无数据时隐藏。

--- src/component/common/BaseSelectionBar/BaseSelectionBar.js
+++ src/component/common/BaseSelectionBar/BaseSelectionBar.js
@@ -5,11 +5,21 @@ export default {
     value: {
       type: Array,
       default: () => ([])
+    },
+    /* 没有数据的时候隐藏 */
+    isNoDataHide: {
+      type: Boolean,
+      default: true,
     }
   },
   data() {
     return {
       visible: false
+    }
+  },
+  computed: {
+    show() {
+      return this.isNoDataHide && Array.isArray(this.value) && this.value.length > 0;
     }
   },
   methods: {
@@ -30,19 +40,23 @@ export default {
   },
   render() {
     return (
-      <div class="base-selection-bar-container">
-        <div class="content">
-            您已选择
-  
-          <span onClick={() => this.$emit('show-panel')} onMouseover={() => this.visible = true} onMouseleave={() => this.visible = false}>
-            {this.value.length}
-            {this.visible && <div class="tooltip">可点击查看详情 <div class="arrow"></div></div>}
-          </span>
-  
-          条数据
-          <span class="clear-btn " onClick={() => this.$emit('toggle-selection')}>清空</span>
-        </div>
-      </div>
+      this.show 
+        ? (
+          <div class="base-selection-bar-container">
+            <div class="content">
+                您已选择
+      
+              <span onClick={() => this.$emit('show-panel')} onMouseover={() => this.visible = true} onMouseleave={() => this.visible = false}>
+                {this.value.length}
+                {this.visible && <div class="tooltip">可点击查看详情 <div class="arrow"></div></div>}
+              </span>
+      
+              条数据
+              <span class="clear-btn " onClick={() => this.$emit('toggle-selection')}>清空</span>
+            </div>
+          </div>
+        )
+        : null
     )
   }
 }
