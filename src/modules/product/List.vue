fd1dfc95c641341ca4894ce3936eb6d7295ef9f5
薄德忠 <mr.bo@bodz.local>
2020-04-08 13:53:21 +0800

    feat:代码优化

--- src/modules/product/List.vue
+++ src/modules/product/List.vue
@@ -388,16 +388,16 @@ export default {
       ];
 
 
-      if (this.initData.productConfig.qrcodeEnabled) {
-        fixedFields.push({
-          displayName: '二维码编号',
-          fieldName: 'qrcodeId',
-          formType: 'text',
-          isExport: false,
-          isSystem: 1,
-          orderId: -4
-        })
-      }
+      // if (this.initData.productConfig.qrcodeEnabled) {
+      //   fixedFields.push({
+      //     displayName: '二维码编号',
+      //     fieldName: 'qrcodeId',
+      //     formType: 'text',
+      //     isExport: false,
+      //     isSystem: 1,
+      //     orderId: -4
+      //   })
+      // }
 
       return (this.initData.productFields || [])
         .concat(fixedFields)
