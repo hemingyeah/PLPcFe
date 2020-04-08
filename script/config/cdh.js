7028b5ef3dca03a7be341432cec7a004d4c3bb60
icaretm <caidh@publink.cn>
2020-04-08 22:15:17 +0800

    fix:修改转交详情页个人备件请求接口，修改事件客户名称

---
+++ script/config/cdh.js
@@ -0,0 +1,7 @@
+module.exports = {
+  targetRootPath: '/Users/publink/web',
+  loginUser: { // 登录用户
+    userId: 'e66ab80f-1588-11ea-bfc9-00163e304a25',
+    tenantId: '7416b42a-25cc-11e7-a500-00163e12f748'
+  }
+}
