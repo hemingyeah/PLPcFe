b58657e34b7a462e271d82e3c01424ba3d904225
bodezhong <13298419015@163.com>
2020-04-20 13:28:02 +0800

    feat:添加bodz打包命令

---
+++ script/config/bodz.js
@@ -0,0 +1,8 @@
+module.exports = {
+    targetRootPath: '/Users/liuxiaohan/Desktop/code/web',
+    loginUser: {
+      // 登录用户
+      userId: '6839d043-683e-11ea-bfc9-00163e304a25',
+      tenantId: '7416b42a-25cc-11e7-a500-00163e12f748'
+    }
+  };
