b3954a905c14d1aafd2f6ef7ecb8ae4198083a4c
phc <2435266246@qq.com>
2020-04-23 11:52:47 +0800

    feat：新增6位短号电话格式校验

--- src/util/validator.js
+++ src/util/validator.js
@@ -5,7 +5,7 @@ export const SINGLE_LINE_MAX_LEN = 50;
 // 多行最大长度
 export const MULTI_LINE_MAX_LEN = 500;
 // 电话
-export const TEL_REG = /^(((0\d{2,3}-{0,1})?\d{7,8}))$/;
+export const TEL_REG = /^(((0\d{2,3}-{0,1})?\d{7,8})|(\d{6}))$/;
 // 手机号
 export const PHONE_REG = /^((1[3578496]\d{9})|([+][0-9-]{1,30}))$/;
 // 日期格式
