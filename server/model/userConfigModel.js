49dd91e12ffaf1635dedfada92f46fa76a560929
黄宝成 <huangbc@publink.cn>
2020-04-27 21:41:01 +0800

    fix: 修改单词拼写错误，智障操作

--- server/model/userConfigModel.js
+++ server/model/userConfigModel.js
@@ -3,7 +3,7 @@ const SCRIPT_PATH = '../../script';
 
 // 当前用户的配置
 const argv = require(`${SCRIPT_PATH}/argv`)(process.argv.slice(2))
-const user = argv.user || 'defalut';
+const user = argv.user || 'default';
 // 售后宝环境
 const SHB_ENV = argv.env && ENVS.includes(argv.env) ? argv.env : '';
 // 用户配置
