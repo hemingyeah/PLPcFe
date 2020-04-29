1d0483da6fabd50370523d414cc0eb1c75830b17
bodezhong <13298419015@163.com>
2020-04-29 19:36:05 +0800

    feat:代码优化

--- src/component/form/components/FormText/FormText.vue
+++ src/component/form/components/FormText/FormText.vue
@@ -6,7 +6,7 @@
       @compositionstart="compositionstart"
       @compositionend="compositionend"
       @input="inputEvent"
-      :placeholder="placeholder"
+      :placeholder="field.placeholder?field.placeholder:placeholder"
       :id="`form_${field.fieldName}`" 
       autocomplete="off"/>
   </div>
