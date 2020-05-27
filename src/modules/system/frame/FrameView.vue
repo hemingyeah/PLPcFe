e2ad1762cf595966c4b3f9537fbe01ff6232af66
黄宝成 <1124778999@qq.com>
2020-05-27 17:10:32 +0800

    fix: 修改帮助文档链接

--- src/modules/system/frame/FrameView.vue
+++ src/modules/system/frame/FrameView.vue
@@ -300,7 +300,7 @@ export default {
       }
     },
     openHelpDoc(event){
-      platform.openLink('https://www.yuque.com/shb');
+      platform.openLink('https://www.yuque.com/shb/help');
       this.profilePopperVisible = false;
     },
     openUserView(event){
