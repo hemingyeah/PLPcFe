b11495bfaae1285a51974f15ebbd3d8bdc9ca17f
黄宝成 <1124778999@qq.com>
2020-05-27 17:11:58 +0800

    feat:增加工作转交详情入口

--- server/routes/jobtransfer.js
+++ server/routes/jobtransfer.js
@@ -5,10 +5,12 @@ const HttpClient = require('../util/HttpClient')
 const modules = require('../../modules');
 const router = new KoaRouter();
 
-// 转交详情
+
 router.get('/jobtransfer/view', async ctx => {
   let script = ['/jobtransfer.detail.js'];
   let modConfig = modules['jobtransfer.detail'];
+  console.info('modConfig:', modConfig);
+  
   let reqHeaders = ctx.request.headers;
 
   // let result = await HttpClient.request(`/security/user/list/${ctx.params.id}`, 'get', null, {headers: reqHeaders});
--- src/modules/jobtransfer/detail.js
+++ src/modules/jobtransfer/detail.js
@@ -1,14 +1,5 @@
 import Vue from '@src/common/entry';
 import TransferDetailView from './views/TransferDetailView.vue';
-import http from '@src/util/http';
-import * as math from 'mathjs';
-
-import VueTour from 'vue-tour';
-require('vue-tour/dist/vue-tour.css');
-Vue.use(VueTour);
-
-Vue.prototype.$http = http;
-Vue.prototype.$math = math;
 // 处理注入的参数
 let initData = {};
 
