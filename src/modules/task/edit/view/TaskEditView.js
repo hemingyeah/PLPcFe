de4dce76da364c4a6a61554e453ace2a7e161ed4
黄宝成 <1124778999@qq.com>
2020-07-02 12:29:02 +0800

    fix: edit task edit view

--- src/modules/task/edit/view/TaskEditView.js
+++ src/modules/task/edit/view/TaskEditView.js
@@ -2,12 +2,14 @@
 import * as TaskApi from '@src/api/TaskApi'
 /* component */
 import TaskEditForm from '@src/modules/task/edit/components/TaskEditForm/TaskEditForm.vue'
-/* model */
-import TaskStateEnum from '@model/enum/TaskStateEnum';
 /* utils */
 import * as FormUtil from '@src/component/form/util'
 import * as util from '@src/modules/task/util/task'
-import platform from '@src/platform'
+import platform from '@src/platform';
+/* vue */
+import data from './data'
+import computed from './computed'
+import methods from './methods'
 
 let taskTemplate = {};
 
@@ -15,137 +17,11 @@ export default {
   name: 'task-edit-view',
   inject: ['initData'],
   data() {
-    return {
-      form: {},
-      init: false,
-      loadingPage: false,
-      pending: false,
-      submitting: false,
-      submitModel: {
-        url: ''
-      }
-    }
+    return data
   },
-  computed: {
-    action() {
-      // TODO: 因为initData无数据 暂时通过url区分新建、编辑
-      let action = location.pathname.indexOf('/task/create') > -1 ? 'create' : 'edit';
-      return this.initData.action || action;
-    },
-    /* 权限 */
-    auth() {
-      let auth = null;
-      try {
-        auth = JSON.parse(this.initData?.auth);
-      } catch (error) {
-        auth = {};
-        console.warn('taskEdit json.parse auth -> error', error);
-      }
-      
-      return auth;
-    },
-    /* 禁用状态 */
-    disabled() {
-      return this.submitting || this.pending;
-    },
-    editId() {
-      // TODO: 因为initData无数据 编辑时暂时取form.id
-      return this.initData?.id || this.form?.id || '';
-    },
-    eventId() {
-      return this.initData?.eventId || '';
-    },
-    /**
-     * @description 是否显示 [返回] 按钮 
-     * 1. 工单编辑有工单id
-     * 2. 
-     * 3. 复制工单
-     * 4. 事件转工单
-    */
-    isShowBackButton() {
-      let path = this.path;
-      return (
-        (path.startsWith('/task/edit/') && path != '/task/edit')
-        || path.startsWith('/task/noFilterEdit/')
-        || path.startsWith('/task/copyTask')
-        || path.startsWith('/event/convent2Task/jump')
-      )
-    },
-    /** 
-     * @description 是否显示 [保存] 按钮
-     * 1. 当前工单状态不为空
-     * 2. 且 当前工单状态 不等于 创建
-    */
-    isShowSaveButton() {
-      return this.workTask.state && this.workTask.state != TaskStateEnum.CREATED.value;
-    },
-    /** 
-     * @description 是否显示 [只保存] 按钮
-     * 1. 当前工单状态 为空
-     * 2. 且 当前工单状态 等于 创建
-    */
-    isShowOnlySaveButton() {
-      return !this.workTask.state || this.workTask.state == TaskStateEnum.CREATED.value;
-    },
-    /** 
-     * @description 是否显示 [保存并派单] 按钮
-     * 1. 当前工单状态 为空
-     * 2. 且 当前工单状态 等于 创建
-     * 3. 有工单派单权限
-    */
-    isShowSaveAndAllotButton() {
-      return this.isShowOnlySaveButton && this.auth.indexOf('TASK_DISPATCH') > -1;
-    },
-    /** 
-     * @description 是否显示工单按钮组
-     * 1. 不是新建计划任务
-     * 2. 不是编辑计划任务
-     */
-    isShowTaskButtonGroup() {
-      return !this.isFromPlan && !this.isPlanTaskEdit;
-    },
-    /* 是否为 来自计划任务 */
-    isFromPlan() {
-      return this.initData?.fromPlan === true;
-    },
-    /* 是否为 来自事件转工单 */
-    isFromEvent() {
-      return this.initData?.fromEve === true;
-    },
-    /* 是否为 编辑计划任务 */
-    isPlanTaskEdit() {
-      return this.initData?.editPlanTask === true;
-    },
-    /* 是否为 工单创建 */
-    isTaskCreate() {
-      return Boolean(this.workTask.id) === false;
-    },
-    /* 是否为 工单编辑 */
-    isTaskEdit() {
-      return Boolean(this.workTask.id) === true;
-    },
-    path() {
-      return this.initData?.path || '';
-    },
-    task() {
-      let task = null;
-      try {
-        task = JSON.parse(this.initData?.task);
-      } catch (error) {
-        task = {};
-        console.warn('taskEdit json.parse task -> error', error);
-      }
-      
-      return task;
-    },
-    types() {
-      return this.initData?.taskTypeList || [];
-    },
-    workTask() {
-      return this.initData?.workTask || {};
-    },
-  },
+  computed,
   methods: {
+    ...methods,
     /** 
      * @description 创建工单方法
     */
@@ -164,7 +40,6 @@ export default {
 
           if (!isSucc) return;
 
-          // this.reloadTab();
           window.location.href = `${res.data}`;
         })
         .catch(err => console.error('err', err))
@@ -210,17 +85,17 @@ export default {
       document.title = title;
     },
     /** 
-     * @description 刷新tab
-    */
+   * @description 刷新tab
+  */
     reloadTab() {
       let fromId = window.frameElement.getAttribute('fromid');
 
       this.$platform.refreshTab(fromId);
     },
     /** 
-     * @description 提交
-     * @param {Boolean} isAllot 是否派单
-    */
+   * @description 提交
+   * @param {Boolean} isAllot 是否派单
+  */
     submit(isAllot) {
       this.submitting = true;
 
@@ -230,11 +105,11 @@ export default {
           this.submitting = false;
 
           if (!valid) return Promise.reject('validate fail.');
-          
+        
           const params = util.packToTask(this.fields, this.form);
           params.templateId = taskTemplate.value;
           params.templateName = taskTemplate.text;
-          
+        
           this.pending = true;
           this.loadingPage = true;
 
@@ -253,8 +128,8 @@ export default {
         })
     },
     /** 
-     * @description 编辑工单方法
-    */
+   * @description 编辑工单方法
+  */
     updateTaskMethod(params) {
       this.$http
         .post(`/task/update?id=${this.editId}`, params)
@@ -277,8 +152,8 @@ export default {
         })
     },
     /** 
-     * @description 更新工单类型
-    */
+   * @description 更新工单类型
+  */
     updateTaskTemplateId(template = {}) {
       taskTemplate = template || {};
     },
