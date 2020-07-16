be42588944e5a47b53de6cb743789c0c6ae16a1b
薄德忠 <13298419015@163.com>
2020-07-16 14:00:06 +0800

    feat:更换办理拒绝 撤销接口为post 表单提交

--- src/api/SparePart.js
+++ src/api/SparePart.js
@@ -4,11 +4,11 @@ let urlHead = '/api/part';
 
 // partV2/repertory/repertoryApply.vue
 export function rejectBatch(params = {}) {
-  return http.get(`${urlHead}/outside/dd/part/approveList/rejectBatch`, params)
+  return http.post(`${urlHead}/outside/dd/part/approveList/rejectBatch`, params, false)
 }
 
 export function revokeBatch(params = {}) {
-  return http.get(`${urlHead}/outside/dd/part/approveList/revokeBatch`, params)
+  return http.post(`${urlHead}/outside/dd/part/approveList/revokeBatch`, params, false)
 }
 
 export function approveBatch(params = {}) {
