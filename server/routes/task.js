a15cd381e239a955d83967ef94179740a09c5bdb
黄宝成 <1124778999@qq.com>
2020-08-31 10:25:21 +0800

    feat: 补全task路由

--- server/routes/task.js
+++ server/routes/task.js
@@ -6,13 +6,11 @@ const router = new KoaRouter();
 const modules = require('../../modules');
 
 router.get('/task', async ctx => {
-  // console.log('task',ctx)
   let script = ['/task.list.js'];
   let modConfig = modules['task.list'];
   let reqHeaders = ctx.request.headers;
   let result = await HttpClient.request('/task', 'get', null, {headers: reqHeaders});
   let body = result.body;
-  // console.log('task', result)
   
   ctx.body = Template.renderWithHtml('工单列表', body, script, modConfig.template)
 });
@@ -24,36 +22,35 @@ router.get('/task/create', async ctx => {
   ctx.body = Template.renderWithData('新建工单', {}, script, modConfig.template)
 });
 
-router.get('/task/edit', async ctx => {
-  let modConfig = modules['task.edit'];
-  let script = ['/task.edit.js'];
-
-  let { url, headers} = ctx.request;
-  let result = await HttpClient.request(url, 'get', null, { headers });
-  let body = result.body;
+router
+  .get('/task/edit', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/edit/:id', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/edit4CallCenter', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/copyTask', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/planTask/edit', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/planTask/create', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/planTask/create', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/task/planTask/edit', async ctx => {
+    await taskEditContent(ctx);
+  })
+  .get('/event/convent2Task/jump', async ctx => {
+    await taskEditContent(ctx);
+  });
   
-  ctx.body = Template.renderWithHtml('编辑工单', body, script, modConfig.template)
-
-}).get('/task/edit/:id', async ctx => {
-  let modConfig = modules['task.edit'];
-  let script = ['/task.edit.js'];
-
-  let { url, headers} = ctx.request;
-  let result = await HttpClient.request(url, 'get', null, { headers });
-  let body = result.body;
-  
-  ctx.body = Template.renderWithHtml('编辑工单', body, script, modConfig.template)
-}).get('/task/edit4CallCenter', async ctx => {
-  let modConfig = modules['task.edit'];
-  let script = ['/task.edit.js'];
-
-  let { url, headers} = ctx.request;
-  let result = await HttpClient.request(url, 'get', null, { headers });
-  let body = result.body;
-  
-  ctx.body = Template.renderWithHtml('新建工单', body, script, modConfig.template)
-});
-
 router.get('/task/view/:id', async ctx => {
   let modConfig = modules['task.view'];
   let reqHeaders = ctx.request.headers;
@@ -79,5 +76,16 @@ router.get('/task/receipt/view/:id', async ctx => {
   
   ctx.body = Template.renderWithHtml('回执表单详情', {}, script, modConfig.template)
 });
+
+async function taskEditContent(ctx) {
+  let modConfig = modules['task.edit'];
+  let script = ['/task.edit.js'];
+
+  let { url, headers} = ctx.request;
+  let result = await HttpClient.request(url, 'get', null, { headers });
+  let body = result.body;
+  
+  ctx.body = Template.renderWithHtml('', body, script, modConfig.template)
+}
 
 module.exports = router;
