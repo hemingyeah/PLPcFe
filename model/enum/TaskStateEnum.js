080de32dfce67858c856b7948b798bbfa4f2316d
庞海翠 <panghc@publink.com>
2020-09-15 11:46:16 +0800

    fix：modify TaskStateEnum.js

--- model/enum/TaskStateEnum.js
+++ model/enum/TaskStateEnum.js
@@ -8,61 +8,67 @@ class TaskStateEnum extends BaseEnum{
     this.CREATED = {
       name: '待指派',
       value: 'created',
-      color: '#69b5f0'
+      color: '#FA8C16'
     }
 
     this.ALLOCATED = {
       name: '已指派',
       value: 'allocated',
-      color: '#1d93ed'
+      color: '#FAAE14'
     }
 
     this.TASK_POOL = {
       name: '工单池',
       value: 'taskPool',
-      color: '#00b5d3'
+      color: '#13C2C2'
     }
 
     this.ACCEPTED = {
       name: '已接受',
       value: 'accepted',
-      color: '#a2db77'
+      color: '#FAAE14'
     }
     
     this.PROCESSING = {
       name: '进行中',
       value: 'processing',
-      color: '#8ecb65'
+      color: '#FAAE14'
     }
 
     this.FINISHED = {
       name: '已完成',
       value: 'finished',
-      color: '#77a842'
+      color: '#67C23A'
     }
     
     this.REFUSED = {
       name: '已拒绝',
       value: 'refused',
-      color: '#ef6b6b'
+      color: '#F56C6C'
     }
 
     this.COSTED = {
       name: '已结算',
       value: 'costed',
-      color: '#ff9933'
+      color: '#67C23A'
     }
     
     this.CLOSED = {
       name: '已关闭',
       value: 'closed',
-      color: '#afafaf'
+      color: '#999999'
     }
 
     this.OFFED = {
       name: '已取消',
       value: 'offed',
-      color: '#cecece'
+      color: '#999999'
+    }
+
+    this.PAUSED = {
+      name: '暂停中',
+      value: 'paused',
+      color: '#F56C6C'
     }
   }
 
@@ -71,6 +77,30 @@ class TaskStateEnum extends BaseEnum{
       let o = this[prop];
       if(o.value == value) return o.color;
     }
+  }
+
+  getNameForTask(task = {}) {
+    if (task.isPaused) return this.PAUSED.name;
+
+    let { state } = task;
+
+    return this.getName(state);
+  }
+
+  getColorForTask(task = {}) {
+    if (task.isPaused) return this.PAUSED.color;
+
+    let { state } = task;
+
+    return this.getColor(state);
+  }
+
+  getValueForTask(task = {}) {
+    if (task.isPaused) return this.PAUSED.value;
+
+    let { state } = task;
+
+    return this.getValue(state);
   }
 }
 
