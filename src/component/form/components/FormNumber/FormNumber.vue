91447db750fa1b2e62fc2fc5081b4b3ac28b62fa
黄宝成 <1124778999@qq.com>
2020-09-24 10:22:40 +0800

    fix: form-number粘贴超长数据修改

--- src/component/form/components/FormNumber/FormNumber.vue
+++ src/component/form/components/FormNumber/FormNumber.vue
@@ -7,13 +7,15 @@
       :id="`form_${field.fieldName}`"
       :placeholder="placeholder" 
       :value="value"
-      @input="input" 
+      @input="input"
     >
   </div>
 </template>
 
 <script>
+
 import FormMixin from '@src/component/form/mixin/form';
+import { FORM_FIELD_TEXT_MAX_LENGTH } from '@src/model/const/Number.ts';
 
 export default {
   name: 'form-number',
@@ -35,6 +37,30 @@ export default {
     nativeInputValue() {
       this.setNativeInputValue();
     },
+  },
+  mounted() {
+    const InputEl = this.$refs.input;
+
+    InputEl.addEventListener('paste', event => {
+      try {
+        let number = event.clipboardData.getData('text')
+        let newValue = number
+        
+        if (number.length > FORM_FIELD_TEXT_MAX_LENGTH) {
+          newValue = number.slice(0, FORM_FIELD_TEXT_MAX_LENGTH)
+        }
+
+        this.$emit('update', { newValue, field: this.field });
+        this.$emit('input', newValue);
+
+      } catch (error) {
+        console.warn('form-number: paste -> error', error)
+      }
+    })
+
+  },
+  beforeDestroy() {
+    this.$refs.input.removeEventListener('paste');
   },
   methods: {
     input(event){
