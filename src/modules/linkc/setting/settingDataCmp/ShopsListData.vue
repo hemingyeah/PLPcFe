586c45a880433e5ee634d0a2d2ed5255cd817567
薄德忠 <13298419015@163.com>
2020-09-27 11:04:13 +0800

    feat:样式优化

--- src/modules/linkc/setting/settingDataCmp/ShopsListData.vue
+++ src/modules/linkc/setting/settingDataCmp/ShopsListData.vue
@@ -2,7 +2,7 @@
   <div class="setting-data-cmp-box">
     <div class="font-16 font-w-500 mar-b-20">商城橱窗</div>
     <div class="text-con">
-      请在前往"查看商品已选项目"配置，允许设立最多4个橱窗商品
+      请前往”查看商品已选项目“配置，最多允许设置4个橱窗商品
       <el-button class="mar-t-24" type="primary" @click="goToShops">去配置</el-button>
     </div>
   </div>
@@ -28,7 +28,7 @@ export default {
   background: #fff;
   .text-con {
     margin-top: 124px;
-    padding: 0 52px;
+    padding: 0 45px;
     color: #666;
     text-align: center;
   }
