8e364a1dadf32abf581acdba64116bc7f5a48ba3
卓尔 <lichao@publink.cn>
2020-11-12 14:11:30 +0800

    feat: 修改自定义字段唯一校验传参

--- src/api/FieldValidateApi.ts
+++ src/api/FieldValidateApi.ts
@@ -7,6 +7,7 @@ let fixedPrefixPath = '/api/app';
  * @param {Object} params - 参数
  * @param {String} params params.fieldName - 自定义字段fieldName
  * @param {String} params params.fieldValue - 自定义字段value
+ * @param {String} params params.id - 客户id （编辑必传）
  */
 export function fieldRepeatCustomer(params: {} | undefined) {
   return http.post(`${fixedPrefixPath}/outside/pc/customer/field/repeat`, params);
@@ -18,6 +19,7 @@ export function fieldRepeatCustomer(params: {} | undefined) {
  * @param {Object} params - 参数
  * @param {String} params params.fieldName - 自定义字段fieldName
  * @param {String} params params.fieldValue - 自定义字段value
+ * @param {String} params params.id - 产品id （编辑必传）
  */
 export function fieldRepeatProduct(params: {} | undefined) {
   return http.post(`${fixedPrefixPath}/outside/pc/product/field/repeat`, params);
