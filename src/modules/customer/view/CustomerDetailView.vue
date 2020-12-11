a989cc70b05978c0919f2736a3097b06b68a8c6e
铁柱 <jinx@publink.cn>
2020-12-11 14:36:23 +0800

    fix:6997，客户编辑后提醒数量为0，产品同理

--- src/modules/customer/view/CustomerDetailView.vue
+++ src/modules/customer/view/CustomerDetailView.vue
@@ -704,6 +704,7 @@ export default {
           this.StatisticalData = res
           this.StatisticalData.callQuantity = callCount
           this.tabs = this.buildTabs()
+          localStorage.setItem('customer_remind_count',res.remindQuantity)
         })
         .catch(err => console.error('fetchStatisticalData', err))
     },
