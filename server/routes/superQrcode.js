96c3fe4503e08c517dc1a3166f69aa285b220ace
铁柱 <jinx@publink.cn>
2020-12-17 22:12:33 +0800

    路由修改

--- server/routes/superQrcode.js
+++ server/routes/superQrcode.js
@@ -5,11 +5,15 @@ const Template = require('../util/Template')
 const router = new KoaRouter();
 const modules = require('../../modules');
 
-router.get('/superQrcode', async ctx => {
+router.get('/setting/superQrcode', async ctx => {
   let script = ['/superQrcode.setting.js'];
   let modConfig = modules['superQrcode.setting'];
+  let reqHeaders = ctx.request.headers;
+
+  let result = await HttpClient.request('/setting/superQrcode', 'get', null, { headers: reqHeaders });
+  let body = result.body;
   
-  ctx.body = Template.renderWithHtml('超级二维码', {}, script, modConfig.template)
+  ctx.body = Template.renderWithHtml('超级二维码', body, script, modConfig.template)
 })
 
 module.exports = router;
