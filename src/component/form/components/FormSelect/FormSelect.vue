b313dfaeaea401b0c0dc7160807b463ecdfd03e4
lily <wangll@publink.cn>
2020-12-17 10:17:44 +0800

    fix:去掉下拉菜单多余判断

--- src/component/form/components/FormSelect/FormSelect.vue
+++ src/component/form/components/FormSelect/FormSelect.vue
@@ -79,7 +79,6 @@ export default {
   computed: {
     isMulti(){
       let setting = this.field.setting || {};
-      if (this.field.formType === 'select' && !this.field.isSystem) return false
       return setting.isMulti;
     },
     options(){
