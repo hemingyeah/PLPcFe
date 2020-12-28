f45c5a0e492036b9b5ffe51c580d277653c2ca0a
铁柱 <jinx@publink.cn>
2020-12-28 17:19:37 +0800

    样式调整

--- server/model/httpConfigModel.js
+++ server/model/httpConfigModel.js
@@ -6,7 +6,7 @@ const DEFAULT_OPIONS = {
   host: '30.40.58.216',
   port: 8090,
   headers:{
-    'cookie':'VIPPUBLINKJSESSIONID=2f90f90e-c2fc-4273-aca2-478804ba2b09'
+    'cookie':'__wpkreporterwid_=77c2d747-071c-4ad6-0416-9c7bec063615; VIPPUBLINKJSESSIONID=0b893c84-0d3b-4899-98c5-564d4e4f4d11; JSESSIONID=9F7EFE01FDFF79F3B6BED8250DFAE4AF'
   }
 };
 // https agent
