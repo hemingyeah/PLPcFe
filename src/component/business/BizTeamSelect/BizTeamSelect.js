29fd679e3ca6d057777cf43bbd6f374e53acd4fe
起点 <caidh@publink.cn>
2021-01-20 10:45:04 +0800

    feat:组织架构调整

--- src/component/business/BizTeamSelect/BizTeamSelect.js
+++ src/component/business/BizTeamSelect/BizTeamSelect.js
@@ -7,7 +7,7 @@ import * as TeamApi from '@src/api/TeamApi'
 import {isEnterprise} from '@src/util/Platform';
 import BaseTreeDept from '../../common/BaseTreeDept';
 import Clickoutside from '@src/util/clickoutside'
-
+import http from '@src/util/http';
 /**
  * TODO: item option render(jsx、template)
 */
@@ -91,11 +91,12 @@ const BizTeamSelect = {
       loading: false,
       page: new Page(),
       params: {
-        pageSize: 50,
-        pageNum: 1,
+        // pageSize: 50,
+        // pageNum: 1,
         keyword: '',
         // 是否只查主团队，true时只查出主团队来，子团队附带在主团队的children里，同时搜索条件支持子团队查询；不传或false时主团队和子团队会在同一级返回
-        onlyParent: true
+        // onlyParent: true
+        seeAllOrg: false
       },
       
       loadmoreOptions: {
@@ -215,11 +216,21 @@ const BizTeamSelect = {
       this.$emit('input', []);
       this.close();
     },
-    
+    getSeeAllOrg() {
+      return http.post('/setting/user/getSeeAllOrg').then(result => { 
+          return result
+      })
+    },
     /** 获取团队数据 */
     async fetchTeam(action){
       try {
         this.loading = true;
+        try {
+          let result = await this.getSeeAllOrg()
+          this.params.seeAllOrg = result.data
+        } catch (error) {
+          this.params.seeAllOrg = true
+        }
         let page = await this.fetchFunc(this.params);
         if (!page) return;
         this.page = page; 
