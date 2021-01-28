571f56f78c1211af8d194da39785069d8327e432
月初 <huangbc@publink.cn>
2021-01-28 14:23:06 +0800

    fix: 修改base select popper css定位

--- config/webpack.deve.conf.js
+++ config/webpack.deve.conf.js
@@ -1,6 +1,5 @@
 /** webpack 开发环境配置  @author dongls */
 const path = require('path')
-const webpack = require('webpack')
 
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.conf')
@@ -10,15 +9,5 @@ module.exports = merge(baseConfig, {
     alias: {
       'app.config': path.resolve(__dirname, '../src/config/development.js'),
     }
-  },
-  plugins: [
-    /**
-     * 1. 使用运行Webpack ProfilingPlugin。
-     * 2. 转到Chrome，打开DevTools，然后转到Performance选项卡（以前为Timeline）。
-     * 3. 将生成的文件（profileEvents.json）拖放到分析器中。然后它将显示时间轴统计信息和每个插件的调用！ 
-    */
-    new webpack.debug.ProfilingPlugin({
-      outputPath: path.join(__dirname, '../profiling/profileEvents.json')
-    })
-  ],
+  }
 })
