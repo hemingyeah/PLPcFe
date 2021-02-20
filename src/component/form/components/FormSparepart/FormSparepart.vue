982f621fa13faf321014a98988be8ac8a70f7059
泽川 <wuzy@publink.cn>
2021-02-20 14:38:19 +0800

    fix：代码优化

--- src/component/form/components/FormSparepart/FormSparepart.vue
+++ src/component/form/components/FormSparepart/FormSparepart.vue
@@ -385,10 +385,10 @@ export default {
         this.value.forEach(val => {
           // id相同时说明是同一个备件 只是自定义选择的不一样
           if (item.id == val.id && val.isAdd) {
-            // originNumber存在 说明是备件列表已存在的备件 库存变动为number减去originNumber
+            // originValue存在 说明是备件列表已存在的备件 库存变动为number减去originValue.number
             let ind = this.originValue.findIndex(_val => _val.id == val.id);
             console.log(ind, this.originValue, this.value, 'originValue 调试111')
-            const num = this.originValue[ind]?.originNumber ? (val.number - this.originValue[ind].originNumber) : val.number
+            const num = this.originValue[ind]?.number ? (val.number - this.originValue[ind].number) : val.number
             maxNum -= num
           }
         })
@@ -522,9 +522,9 @@ export default {
         this.value.forEach(val => {
           // id相同时说明是同一个备件 只是自定义选择的不一样
           if (this.sparepart.id == val.id && val.isAdd) {
-            // originNumber存在 说明是备件列表已存在的备件 库存变动为number减去originNumber
+            // originValue存在 说明是备件列表已存在的备件 库存变动为number减去originValue.number
             let ind = this.originValue.findIndex(_val => _val.id == val.id);
-            const num = this.originValue[ind]?.originNumber ? (val.number - this.originValue[ind].originNumber) : val.number
+            const num = this.originValue[ind]?.number ? (val.number - this.originValue[ind].number) : val.number
             maxNum -= num
           }
         })
@@ -681,13 +681,7 @@ export default {
         })
         this.partField = result.result || []
         if (this.value && this.value.length) {
-          this.originValue = []
-          this.value.forEach(val => {
-            // 新增originNumber保存原先的备件数量
-            let obj = Object.assign({}, val)
-            obj.originNumber = val.number || 0
-            this.originValue.push(obj)
-          })
+          this.originValue = _.cloneDeep(this.value)
           console.log(this.originValue, 'originValue断点')
         }
       }
