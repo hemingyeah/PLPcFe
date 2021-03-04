41211df0c68d336a9dda8e70a465ada43dfdf1ae
月初 <huangbc@publink.cn>
2021-03-04 18:59:11 +0800

    fix: 修改工单编辑 附件数据处理

--- src/modules/task/edit/view/TaskEditView.js
+++ src/modules/task/edit/view/TaskEditView.js
@@ -480,9 +480,11 @@ export default {
           
           this.togglePending(true);
           
-          if (this.isTaskEdit && Array.isArray(task.attachment)) {
+          if (this.isTaskEdit) {
             // fix: 后端会查询回执附件拼接上去，所以前端需要过滤下回执附件
-            params.task.attachment = task.attachment.filter(item => !item.receipt);
+            if (Array.isArray(task.attachment)) {
+              params.task.attachment = task.attachment.filter(item => !item.receipt)
+            }
             return this.updateTaskMethod(params, isAllot);
           }
           if (this.isTaskCreate) {
